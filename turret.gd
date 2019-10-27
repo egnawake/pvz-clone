2771f256b07f22bacf8e517b25c0514cd6342495
João Inácio <egnawake@gmail.com>
2019-10-27 14:43:40 +0000

    Add attack animation

--- turret.gd
+++ turret.gd
@@ -8,14 +8,7 @@ func _ready():
 func setup():
     $AttackTimer.wait_time = 1.0 / attack_rate
     $AttackTimer.connect("timeout", self, "attack")
-    $Attack/EffectTimer.connect("timeout", self, "finish_attack")
     $AttackTimer.start()
 
 func attack():
-    $Attack/AttackHitbox.disabled = false
-    $Attack/AttackEffect.visible = true
-    $Attack/EffectTimer.start()
-
-func finish_attack():
-    $Attack/AttackHitbox.disabled = true
-    $Attack/AttackEffect.visible = false
+    $Animator.play("attack")
