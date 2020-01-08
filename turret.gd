5a9d184dc4f6dba51a4683d6f5fd6a3c32a8dafd
João Inácio <egnawake@gmail.com>
2020-01-08 00:51:30 +0000

    Add ranged attack to turrets

--- turret.gd
+++ turret.gd
@@ -1,14 +1,26 @@
 extends Node2D
 
-export (float) var attack_rate = 1.0 # attacks per second
+export var attack_rate = 1.0 # attacks per second
+export var is_ranged = false
+
+onready var attack_timer = $AttackTimer
+onready var attack_ray = $AttackRay
 
 func _ready():
-    setup()
+    start_attack_timer()
 
-func setup():
-    $AttackTimer.wait_time = 1.0 / attack_rate
-    $AttackTimer.connect("timeout", self, "attack")
-    $AttackTimer.start()
+func start_attack_timer():
+    var attack_type = "ranged_attack" if is_ranged else "attack"
+    attack_timer.wait_time = 1.0 / attack_rate
+    attack_timer.connect("timeout", self, attack_type)
+    attack_timer.start()
 
 func attack():
     $Animator.play("attack")
+
+func ranged_attack():
+    if attack_ray.is_colliding():
+        var enemy = attack_ray.get_collider().get_parent()
+        if enemy.has_method('take_damage'):
+            enemy.take_damage(5)
+    
